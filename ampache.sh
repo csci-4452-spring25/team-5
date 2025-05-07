72456d6ec63a81d51c9515c19b5dc70e5a22bde2
ajferna-3 <ajferna3@uno.edu>
2025-05-07 18:54:06 -0500

    Update ampache.sh

--- ampache.sh
+++ ampache.sh
@@ -25,7 +25,7 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get install awscli docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
